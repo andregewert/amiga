f81e3aca539200ac0e2a3cbf5cd883d3c15f05c5
André Gewert <agewert@ubergeek.de>
2023-08-06 02:08:17 +0200

    Renamed tests; added structures for SystemBar

---
+++ tools/src/systembar/README.md
@@ -0,0 +1,6 @@
+# SystemBar
+
+Simple window switcher and start menu for Amiga systems.
+
+## Compatibility
+This tool uses some intuition functions which were introduced with AmigaOS 3.2 - so other versions than AmigaOS 3.2 are not supported at the moment.
---
+++ tools/src/systembar/functions.c
@@ -0,0 +1,90 @@
+/*
+ Copyright (c) 2023 André Gewert <agewert@ubergeek.de>
+
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#include "functions.h"
+
+struct Window* sbFindWindowByName(CONST STRPTR windowName) {
+    struct Screen* wbScreen = LockPubScreen("Workbench");
+    struct Window* foundWindow = NULL;
+
+    if (wbScreen != NULL) {
+        struct Window* window = wbScreen->FirstWindow;
+        while (window != NULL) {
+            if (!strcmp(window->Title, windowName)) {
+                foundWindow = window;
+                break;
+            }
+            window = window->NextWindow;
+        }
+    }
+    UnlockPubScreen(NULL, wbScreen);
+    return foundWindow;
+}
+
+BOOL sbCloseWindow(struct Window* windowToClose) {
+    BOOL closed = FALSE;
+    struct Screen* wbScreen = LockPubScreen("Workbench");
+    if (wbScreen != NULL) {
+        struct Window* window = wbScreen->FirstWindow;
+        while (window != NULL) {
+            if (window == windowToClose) {
+                CloseWindow(windowToClose);
+                closed = TRUE;
+                break;
+            }
+            window = window->NextWindow;
+        }
+    }
+    UnlockPubScreen(NULL, wbScreen);
+    return closed;
+}
+
+BOOL sbHideWindow(struct Window* windowToHide) {
+    BOOL closed = FALSE;
+    struct Screen* wbScreen = LockPubScreen("Workbench");
+    if (wbScreen != NULL) {
+        struct Window* window = wbScreen->FirstWindow;
+        while (window != NULL) {
+            if (window == windowToHide) {
+                HideWindow(windowToHide);
+                closed = TRUE;
+                break;
+            }
+            window = window->NextWindow;
+        }
+    }
+    UnlockPubScreen(NULL, wbScreen);
+    return closed;
+}
+
+BOOL sbUnhideWindow(struct Window* windowToUnhide) {
+    BOOL closed = FALSE;
+    struct Screen* wbScreen = LockPubScreen("Workbench");
+    if (wbScreen != NULL) {
+        struct Window* window = wbScreen->FirstWindow;
+        while (window != NULL) {
+            if (window == windowToUnhide) {
+                ShowWindow(windowToUnhide);
+                closed = TRUE;
+                break;
+            }
+            window = window->NextWindow;
+        }
+    }
+    UnlockPubScreen(NULL, wbScreen);
+    return closed;
+}
