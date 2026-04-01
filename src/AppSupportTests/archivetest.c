fbc21fefbc190cd0d5d8b5c5a1544846ca265c0d
André Gewert <agewert@ubergeek.de>
2026-04-01 21:55:15 +0200

    Small structure changes

--- src/AppSupportTests/archivetest.c
+++ src/AppSupportTests/archivetest.c
@@ -30,137 +30,8 @@ void createDummyFile(const char* filename, const char* content) {
 }
 
 int main() {
-    testInit("Archive Tests");
-    const char* archiveName = "T:test.arch";
-    const char* file1 = "T:file1.txt";
-    const char* file2 = "T:file2.txt";
-    const char* file1_content = "Hello, this is file 1 content!";
-    const char* file2_content = "And this is file 2 content, which is slightly longer.";
-
-    printf("Creating dummy files...\n");
-    createDummyFile(file1, file1_content);
-    createDummyFile(file2, file2_content);
-
-    printf("Opening archive %s...\n", archiveName);
-    Archive* arch = archiveOpen(archiveName);
-    ASSERT_NOT_NULL(arch);
-    if (!arch) {
-        return testSummary();
-    }
-
-    printf("Adding %s to archive...\n", file1);
-    ASSERT_TRUE(archiveAddFile(arch, file1, "file1.txt"));
-
-    printf("Adding %s to archive...\n", file2);
-    ASSERT_TRUE(archiveAddFile(arch, file2, "file2.txt"));
-
-    archiveClose(arch);
-    printf("Archive closed.\n");
-
-    printf("Re-opening archive for verification...\n");
-    arch = archiveOpen(archiveName);
-    ASSERT_NOT_NULL(arch);
-    if (!arch) {
-        return testSummary();
-    }
-
-    linkedList* toc = archiveGetTOC(arch);
-    ASSERT_NOT_NULL(toc);
-    printf("Table of Contents:\n");
-    for (uint32_t i = 0; i < 100; i++) {
-        listElement* el = listGetElementAt(toc, i);
-        if (!el) break;
-        ArchiveEntry* entry = (ArchiveEntry*)el->data;
-        printf("- %s (Size: %u, Offset: %u)\n", entry->fileName, entry->size, entry->offset);
-    }
-
-    printf("Reading file1.txt from archive...\n");
-    uint32_t size1;
-    char* data1 = (char*)archiveReadFile(arch, "file1.txt", &size1);
-    ASSERT_NOT_NULL(data1);
-    if (data1) {
-        printf("Content: %.*s\n", (int)size1, data1);
-        ASSERT_INT_EQ(strlen(file1_content), size1);
-        ASSERT_STR_EQ(file1_content, data1);
-        free(data1);
-    }
-
-    printf("Extracting file2.txt to T:extracted_file2.txt...\n");
-    ASSERT_TRUE(archiveExtractFile(arch, "file2.txt", "T:extracted_file2.txt"));
-
-    printf("Testing existence check...\n");
-    ASSERT_TRUE(archiveFileExists(arch, "file1.txt"));
-    ASSERT_TRUE(archiveFileExists(arch, "file2.txt"));
-    ASSERT_FALSE(archiveFileExists(arch, "nonexistent.txt"));
-
-    printf("Testing replacement prevention: adding file1.txt again should fail...\n");
-    ASSERT_FALSE(archiveAddFile(arch, file1, "file1.txt"));
-
-    printf("Testing replacement: updating file1.txt in archive using archiveReplaceFile...\n");
-    const char* file1_new_content = "This is the NEW content for file 1, it is much longer than before.";
-    createDummyFile(file1, file1_new_content);
-    ASSERT_TRUE(archiveReplaceFile(arch, file1, "file1.txt"));
-
-    linkedList* new_toc = archiveGetTOC(arch);
-    uint32_t count = 0;
-    for (uint32_t i = 0; i < 100; i++) {
-        listElement* el = listGetElementAt(new_toc, i);
-        if (!el) break;
-        count++;
-    }
-    ASSERT_INT_EQ(2, count);
-
-    printf("Reading NEW file1.txt from archive...\n");
-    uint32_t new_size1;
-    char* new_data1 = (char*)archiveReadFile(arch, "file1.txt", &new_size1);
-    ASSERT_NOT_NULL(new_data1);
-    if (new_data1) {
-        ASSERT_INT_EQ(strlen(file1_new_content), new_size1);
-        ASSERT_STR_EQ(file1_new_content, new_data1);
-        free(new_data1);
-    }
-
-    printf("Testing path support...\n");
-    const char* file_with_path = "T:subdir/file3.txt";
-    const char* entry_with_path = "subdir/file3.txt";
-    const char* content3 = "File in a subdirectory";
-
-    // Ensure directory for dummy file exists
-    BPTR dirLock = CreateDir((STRPTR)"T:subdir");
-    if (dirLock) UnLock(dirLock);
-
-    createDummyFile(file_with_path, content3);
-    
-    ASSERT_TRUE(archiveAddFile(arch, file_with_path, entry_with_path));
-
-    printf("Testing maximum filename length...\n");
-    char long_filename[ARCHIVE_MAX_FILENAME + 10];
-    memset(long_filename, 'a', sizeof(long_filename) - 1);
-    long_filename[sizeof(long_filename) - 1] = '\0';
-    ASSERT_FALSE(archiveAddFile(arch, file1, long_filename));
-
-    char max_filename[ARCHIVE_MAX_FILENAME];
-    memset(max_filename, 'b', sizeof(max_filename) - 1);
-    max_filename[sizeof(max_filename) - 1] = '\0';
-    ASSERT_TRUE(archiveAddFile(arch, file1, max_filename));
-    ASSERT_TRUE(archiveFileExists(arch, max_filename));
-
-    printf("Extracting %s to T:extracted_subdir/file3.txt...\n", entry_with_path);
-    // This extraction should create T:extracted_subdir/
-    ASSERT_TRUE(archiveExtractFile(arch, entry_with_path, "T:extracted_subdir/file3.txt"));
-
-    printf("Verifying extracted file...\n");
-    uint32_t size3;
-    char* data3 = (char*)archiveReadFile(arch, entry_with_path, &size3);
-    ASSERT_NOT_NULL(data3);
-    if (data3) {
-        ASSERT_INT_EQ(strlen(content3), size3);
-        ASSERT_STR_EQ(content3, data3);
-        free(data3);
-    }
-
-    printf("Testing recursive directory addition...\n");
-    // Create a directory structure: T:recursive/fileA.txt, T:recursive/subdir/fileB.txt
+    testInit("Archive ShelltoolsTests");
+    const char* archiveName = "T:test2.arch";
     const char* rec_dir = "T:recursive";
     const char* rec_subdir = "T:recursive/subdir";
     const char* fileA = "T:recursive/fileA.txt";
@@ -176,9 +47,24 @@ int main() {
     createDummyFile(fileA, contentA);
     createDummyFile(fileB, contentB);
 
+    Archive* arch = archiveOpen(archiveName);
+    ASSERT_NOT_NULL(arch);
     ASSERT_TRUE(archiveAddDirectory(arch, rec_dir, "rec"));
+    archiveClose(arch);
 
-    printf("Verifying recursive addition...\n");
+    arch = archiveOpen(archiveName);
+    ASSERT_NOT_NULL(arch);
+
+    linkedList* toc = archiveGetTOC(arch);
+    ASSERT_NOT_NULL(toc);
+
+    printf("Table of Contents:\n");
+    for (uint32_t i = 0; i < 100; i++) {
+        listElement* el = listGetElementAt(toc, i);
+        if (!el) break;
+        ArchiveEntry* entry = (ArchiveEntry*)el->data;
+        printf("- %s (Size: %u, Offset: %u)\n", entry->fileName, entry->size, entry->offset);
+    }
     ASSERT_TRUE(archiveFileExists(arch, "rec/fileA.txt"));
     ASSERT_TRUE(archiveFileExists(arch, "rec/subdir/fileB.txt"));
 
@@ -197,21 +83,7 @@ int main() {
         free(dB);
     }
 
-    printf("Testing archiveExtractAll...\n");
-    const char* extract_dir = "T:extracted_all";
-    // CreateDir(extract_dir) is not strictly needed if archiveExtractFile handles it,
-    // but extracting all to a single root might be cleaner if it's already there.
-    // Actually, createPath in archiveExtractFile handles it.
-    
-    ASSERT_TRUE(archiveExtractAll(arch, extract_dir));
-
-    printf("Verifying extracted contents...\n");
-    ASSERT_TRUE(archiveFileExists(arch, "file1.txt")); // still in archive
-    
-    // Check files on disk (manual verification by reading them back from archive for comparison)
-    // In a real test we would use Lock/Examine or Open to check disk existence.
-    // Here we can at least verify the function returned TRUE.
-    
     archiveClose(arch);
+
     return testSummary();
 }
